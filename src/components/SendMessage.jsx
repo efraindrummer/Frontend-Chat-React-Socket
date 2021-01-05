b7a43d30e6331bb08e2a622c1c790846c648bcce
Efrain May <efrain_may_75@hotmail.com>
2021-01-05 01:05:47 -0600

    enviar mensajes desde DE a PARA localizando el id

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { ChatContext } from "../context/chat/ChatContext";
+import { SocketContext } from "../context/SocketContext";
 
 export const SendMessage = () => {
+
+    const [mensaje, setMensaje] = useState('');
+    const { socket } = useContext(SocketContext);
+    const { auth } = useContext(AuthContext);   
+    const { chatState } = useContext(ChatContext);
+
+    const onChange = ({target}) => {
+        setMensaje(target.value);
+    }
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        if(mensaje.length === 0){ return; }
+        setMensaje('');
+
+        //emitir un evento del socket parta enviar los mensajes
+        /*{ 
+            de: //usuario enviando los mensajes
+            para: //uid del usuario que recibe el usuario
+            mensaje: //el mensaj que quiero enviar
+        } */
+        socket.emit('mensaje-personal', {
+            de: auth.uid,
+            para: chatState.chatActivo,
+            mensaje
+        });
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="type_msg row">
             <div className="input_msg_write col-sm-9">
-                <input type="text" className="write_msg" placeholder="Mensaje..." />
+                <input 
+                    type="text" 
+                    className="write_msg" 
+                    placeholder="Mensaje..." 
+                    value={mensaje}
+                    onChange={onChange}    
+                />
             </div>
             <div className="col-sm-3 text-center">
                 <button className="msg_send_btn mt-3" type="submit">
